7ffc2e2248c7f92a24e3d06733e36194427b7684
ssbolsta <syver92@msn.com>
2014-10-07 16:05:06 +0200

    nyeste

---
+++ src/gui/Koier.java
@@ -0,0 +1,28 @@
+package gui;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Koier extends Application{
+	
+	
+	
+
+	
+
+		@Override
+		public void start(Stage primaryStage) throws IOException {
+			Parent root = FXMLLoader.load(Koier.class.getResource("Koier.fxml"));
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		}
+
+		public static void main(String[] args) {
+			launch(args);
+		}
+}
+
