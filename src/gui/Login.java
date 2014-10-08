bd77edeb3f9b0663fe71016fa90f6005f54fca38
ssbolsta <syver92@msn.com>
2014-10-08 12:12:16 +0200

    Tilbakeknapp
    lagt inn tilbakeknapp og overskrift på logg inn

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,4 +1,5 @@
 package gui;
+import java.io.File;
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -8,19 +9,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Login extends Application{
 	private Stage primaryStage;
 	
-
+	@FXML
+	private ImageView imageView;
 	@Override
 	public void start(Stage primaryStage) throws IOException {
+	
 	    FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setController(this);
         Parent root = (Parent) fxmlLoader.load(this.getClass().getResourceAsStream("Login.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+     
         this.primaryStage = primaryStage;
 	}
 
