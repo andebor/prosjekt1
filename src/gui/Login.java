8fc4e2f62f2dea2942b0d019ce4469638b8e93b4
obratvedt <oyvindbratvedt@hotmail.com>
2014-10-08 14:40:14 +0200

    Picture and button fix in login

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -1,17 +1,10 @@
 package gui;
 
-import java.io.File;
 import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
@@ -24,7 +17,7 @@ public class Login extends Application {
 	    primaryStage.setScene(new Scene(root));
 	    primaryStage.show();
 	    LoginController lc = new LoginController();
-	    lc.primaryStage = primaryStage;
+	    lc.setPrimaryStage(primaryStage);
 	}
 	 
 	
