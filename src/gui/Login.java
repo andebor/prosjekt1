e69b7a296f5950c5f41eeff660ccfddfb6c58b0e
Joergen Bolli <rundohf@gmail.com>
2014-10-10 17:26:36 +0200

    Koblet kart til main menu

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -7,19 +7,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Login extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		
 		Parent root = FXMLLoader.load(this.getClass().getResource("Login.fxml"));
 	    primaryStage.setScene(new Scene(root));
 	    primaryStage.show();
 	    LoginController.setPrimaryStage(primaryStage);
 	}
-	 
-	
 	
 	public static void main(String[] args) {
 		launch(args);
