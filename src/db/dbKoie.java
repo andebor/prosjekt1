c532c63259247b8b547895cf0a1964d46154ae69
hegebor <hegebor@stud.ntnu.no>
2014-10-10 18:29:05 +0200

    dbkoie

---
+++ src/db/dbKoie.java
@@ -0,0 +1,31 @@
+package db;
+
+import java.sql.ResultSet;
+
+public class dbKoie {
+	
+	public static void main(String[] args) {
+//        System.out.println("main");
+//        new dbKoie().getKoie();
+//
+    }
+//
+//    public dbKoie() {
+//    }
+//
+//    private Koie getKoie(String koiename, int numberOfBeds, String description, Image image) {
+//        String sql = "select * from koie where koie_name = ?";
+//        Koie koie = null;
+//        try (PreparedStatement ps = DbConnection.getInstance().prepareStatement(sql)) {
+//        	ResultSet rs = ps.executeQuery();
+//            while (rs.next()) {
+//                koie = new Koie(rs.getString("koie_name"), rs.getInt(numberOfBeds), rs.getString("description"), );
+//                if (withPassword) employee.setPassword(rs.getString("passord"));
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return koie;
+//    }
+    
+}
