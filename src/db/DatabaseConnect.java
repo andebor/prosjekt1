f4e890f4d9d74ff1aaca69a8b3af8b1436bba01c
obratvedt <oyvindbratvedt@hotmail.com>
2014-10-14 15:11:41 +0200

    Login db connection

--- src/db/DatabaseConnect.java
+++ src/db/DatabaseConnect.java
@@ -1,4 +1,4 @@
-package koier;
+package db;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
--- src/db/DbAdmin.java
+++ src/db/DbAdmin.java
@@ -1,11 +1,32 @@
 package db;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.sun.crypto.provider.RSACipher;
+
+
+
 public class DbAdmin {
 
 	public boolean checkAdmin(String username, String pw){
-		
+		String sql = "select password from admin where admin_email = ?";
+		try (PreparedStatement ps = DatabaseConnect.getInstance().prepareStatement(sql)){
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()){
+				String dbPw = rs.getString("password");
+				if (dbPw.equals(pw)){
+					return true;
+				}
+			}
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+	
 		return false;
-		
 	}
 	
 	
