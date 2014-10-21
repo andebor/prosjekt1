071c194185f31e5f9e19e8f2deed5f08751b2441
hegebor <hegebor@stud.ntnu.no>
2014-10-21 20:04:56 +0200

    h

---
+++ src/model/ModelReports.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.util.Date;
+
+public class ModelReports {
+		
+		private int report_id;
+		private String koieName;
+		private boolean status;
+		private Date startDate;
+		private Date endDate;
+		private boolean smokeDetector;
+		private boolean wood;
+		private String remarksOfDefects;
+		private boolean forgotten;
+		private String comments;
+		private Date timeStamp;
+		 
+
+		public ModelReports(int report_id, String koie_name, boolean status, Date startdate, Date enddate, boolean smoke_detector, 
+				boolean wood, String remarks_of_defects, boolean forgotten, String comments, Date timeStamp){
+			this.report_id = report_id;
+			this.koieName = koie_name;
+			this.status = status;
+			this.startDate = startdate;
+			this.endDate = enddate;
+			this.smokeDetector = smoke_detector;
+			this.wood = wood;
+			this.remarksOfDefects = remarks_of_defects;
+			this.forgotten = forgotten;
+			this.comments = comments;
+			this.timeStamp = timeStamp;
+		}
+
+		public String getKoieName() {
+			return koieName;
+		}
+
+		public Date getStartDate() {
+			return startDate;
+		}
+		
+		public Date getEndDate(){
+			return endDate;
+		}
+		public boolean getWood() {
+			return wood;
+		}
+		
+		public boolean getStatus(){
+			return status;
+		}
+		
+		public Date getTimeStamp(){
+			return timeStamp;
+		}
+
+
+}
