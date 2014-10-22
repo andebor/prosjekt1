763b5ecea5f90dd91017172153a4f5f959faa5d8
hegebor <hegebor@stud.ntnu.no>
2014-10-22 15:42:35 +0200

    Lagt til alt til utstyrslista for alle koier

--- src/database/DbEquipmentList.java
+++ src/database/DbEquipmentList.java
@@ -3,10 +3,29 @@ package database;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.ModelEquipmentList;
+import model.ModelEquipmentLists;
+import model.ModelReservations;
 
 public class DbEquipmentList {
+	
+	public static List<ModelEquipmentLists> getEquipmentLists() {
+        String sql = "select * from current_inventory";
+        List<ModelEquipmentLists> equipments = new ArrayList<>();
+        try (PreparedStatement ps = DatabaseConnect.getInstance().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+            	ModelEquipmentLists equipment = new ModelEquipmentLists(rs.getString("koie"), rs.getInt("wood"), rs.getInt("status"));
+            	equipments.add(equipment);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return equipments;
+    }
 	
 	private ModelEquipmentList getEquipmentList(String koieName) {
         String sql = "select * from equipment_list where koie_name = ?";
