5718f59cabac02589d06b0abaa38ae223953b6f1
obratvedt <oyvindbratvedt@hotmail.com>
2014-10-24 14:07:09 +0200

    Unit test dbreports

--- tests/database/DbReports_Test.java
+++ tests/database/DbReports_Test.java
@@ -1,5 +1,22 @@
 package database;
 
-public class DbReports_Test {
+import static org.junit.Assert.assertEquals;
+import model.ModelReports;
 
+import org.junit.Test;
+
+public class DbReports_Test {
+	DbReports dbReport = new DbReports();
+	
+	//Note: Might not be useful before we have sufficient data in the db
+	
+	@Test
+	public void testGetReservationWhenIdExists(){
+		ModelReports report = dbReport.getReport(18);
+		int expected = 18;
+		int actual = report.getReportId();
+		assertEquals(expected,actual);
+	}
+	
+	
 }
