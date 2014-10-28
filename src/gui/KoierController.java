2c9e1ac0d3dbe3f85a2646faa3439f43bf3846f2
obratvedt <oyvindbratvedt@hotmail.com>
2014-10-28 11:33:27 +0100

    yolo

--- src/gui/KoierController.java
+++ src/gui/KoierController.java
@@ -1,12 +1,17 @@
 package gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
-public class KoierController {
+public class KoierController implements Initializable {
 
 	private static Stage primaryStage;
 	
@@ -16,10 +21,18 @@ public class KoierController {
 	
 	@FXML
 	private Button back;
+	@FXML
+	private ChoiceBox koieList;
 	
 	@FXML
 	public void backToMainMenu(ActionEvent event) throws IOException{
 		MainMenu mm = new MainMenu();
 		mm.start(primaryStage);
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
 	}
 }
