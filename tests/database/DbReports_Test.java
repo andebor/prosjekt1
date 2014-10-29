ce778f64c07a634ba94fae3571837b08d355b5d6
obratvedt <oyvindbratvedt@hotmail.com>
2014-10-29 18:30:59 +0100

    Bakgrunnsfarger tekstfelt på levert rapport/ikke

--- tests/database/DbReports_Test.java
+++ tests/database/DbReports_Test.java
@@ -8,15 +8,4 @@ import org.junit.Test;
 public class DbReports_Test {
 	DbReports dbReport = new DbReports();
 	
-	//Note: Might not be useful before we have sufficient data in the db
-	
-	@Test
-	public void testGetReservationWhenIdExists(){
-		ModelReports report = dbReport.getReport(18);
-		int expected = 18;
-		int actual = report.getReportId();
-		assertEquals(expected,actual);
-	}
-	
-	
 }
