e01900e7d8ec3373dfd272af6066770a161f2697
obratvedt <oyvindbratvedt@hotmail.com>
2014-11-04 10:23:27 +0100

    Fjernet ubrukte imports og forbedret logikk i email

--- src/gui/ReservationController.java
+++ src/gui/ReservationController.java
@@ -5,22 +5,15 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.sql.Date;
 import java.util.ResourceBundle;
 
 import model.ModelReservations;
 import database.DbReports;
-import database.DbReservations;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class ReservationController implements Initializable{
@@ -55,32 +48,32 @@ public class ReservationController implements Initializable{
 	}
 	@FXML
 	public void sendStandardEmail(ActionEvent event){
-		 Desktop desktop = Desktop.getDesktop(); 
-		 try {
-			 String body = "Hei%0DDu%20har%20reservert"
-			 		+ "%20"+reservation.getKoieName()+"%20i%20perioden%20"+reservation.getStartDate().toString()
-			 		+"%20til%20"+reservation.getEndDate().toString()+"%0D"
-			 		+ "Husk%20å%20sende%20inn%20rapport%20etter%20oppholdet%20på%20http://org.ntnu.no/gruppetre/%0D"
-			 		+"Hilsen%20Koiestyret";
-			desktop.mail(new URI("mailto:"+email.getText()+"?subject=Koierapport&body="+body));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		sendEmail(true);
 	}
 	@FXML
 	public void sendCustomEmail(ActionEvent event){
-		Desktop desktop = Desktop.getDesktop();
-		try{
-			desktop.mail(new URI("mailto:"+email.getText()));
+		sendEmail(false);
+	}
+	
+	private void sendEmail(boolean standard){
+		 Desktop desktop = Desktop.getDesktop(); 
+		 try {
+			 if (standard){
+				 String body = "Hei%0DDu%20har%20reservert"
+					 		+ "%20"+reservation.getKoieName()+"%20i%20perioden%20"+reservation.getStartDate().toString()
+					 		+"%20til%20"+reservation.getEndDate().toString()+"%0D"
+					 		+ "Husk%20å%20sende%20inn%20rapport%20etter%20oppholdet%20på%20http://org.ntnu.no/gruppetre/%0D"
+					 		+"Hilsen%20Koiestyret";
+				desktop.mail(new URI("mailto:"+email.getText()+"?subject=Koierapport&body="+body));
+			 }
+			 else{
+				 desktop.mail(new URI("mailto:"+email.getText()));
+			 }
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
