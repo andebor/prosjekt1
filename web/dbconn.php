99b1920a35da9aa94a285a0d29ab6b38d079f92a
andebor <post@andersborud.com>
2014-11-05 16:19:34 +0100

    Added email check on form. Also changed how the site connects to db

---
+++ web/dbconn.php
@@ -0,0 +1,14 @@
+<?php
+// Connect to database@
+
+$dbhost = 'mysql.stud.ntnu.no';
+$dbuser = 'oyvbr_prosjekt1';
+$dbpass = 'oyvbr_prosjekt1';
+$dbname = 'oyvbr_koie';
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+?>
