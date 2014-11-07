3085d8e91e6b6d8f9ab53b565a65da4d8dd408d6
hegebor <hegebor@stud.ntnu.no>
2014-11-07 15:20:45 +0100

    db equipment

--- src/database/DbEquipmentList.java
+++ src/database/DbEquipmentList.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import model.ModelEquipment;
 import model.ModelEquipmentLists;
-import model.ModelReports;
 
 public class DbEquipmentList {
 	
@@ -56,9 +55,49 @@ public class DbEquipmentList {
         }
         return equipments;
     }
-
+	
 			
-			
+	public static List<ModelEquipment> getEquipmentStatus() {
+        String sql = "select * from current_inventory2 where utstyr = 'status'";
+        List<ModelEquipment> inventory = new ArrayList<>();
+        try (PreparedStatement ps = DatabaseConnect.getInstance().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+            	ModelEquipment status = new ModelEquipment(rs.getInt("id"), rs.getString("utstyr"), rs.getInt("Flåkoia"), 
+            			rs.getInt("Fosenkoia"), rs.getInt("Heinfjordstua"), rs.getInt("Hognabu"), rs.getInt("Holmsåkoia"), 
+            			rs.getInt("Holvassgamma"), rs.getInt("Iglbu"), rs.getInt("Kamtjønnkoia"), rs.getInt("Kråklikåten"), 
+            			rs.getInt("Kvernmovollen"), rs.getInt("Kåsen"), rs.getInt("Lynhøgen"), rs.getInt("Mortenskåten"), 
+            			rs.getInt("Nicokoia"), rs.getInt("Rindalsløa"), rs.getInt("Selbukåten"), rs.getInt("Sonvasskoia"), 
+            			rs.getInt("Stabburet"), rs.getInt("Stakkslettbua"), rs.getInt("Telin"), rs.getInt("Taagaabu"), 
+            			rs.getInt("Vekvessætra"), rs.getInt("Øvensenget"));
+            	inventory.add(status);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return inventory;
+    }		
+	
+	public static List<ModelEquipment> getEquipmentWood() {
+        String sql = "select * from current_inventory2 where utstyr = 'wood'";
+        List<ModelEquipment> inventory = new ArrayList<>();
+        try (PreparedStatement ps = DatabaseConnect.getInstance().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+            	ModelEquipment wood = new ModelEquipment(rs.getInt("id"), rs.getString("utstyr"), rs.getInt("Flåkoia"), 
+            			rs.getInt("Fosenkoia"), rs.getInt("Heinfjordstua"), rs.getInt("Hognabu"), rs.getInt("Holmsåkoia"), 
+            			rs.getInt("Holvassgamma"), rs.getInt("Iglbu"), rs.getInt("Kamtjønnkoia"), rs.getInt("Kråklikåten"), 
+            			rs.getInt("Kvernmovollen"), rs.getInt("Kåsen"), rs.getInt("Lynhøgen"), rs.getInt("Mortenskåten"), 
+            			rs.getInt("Nicokoia"), rs.getInt("Rindalsløa"), rs.getInt("Selbukåten"), rs.getInt("Sonvasskoia"), 
+            			rs.getInt("Stabburet"), rs.getInt("Stakkslettbua"), rs.getInt("Telin"), rs.getInt("Taagaabu"), 
+            			rs.getInt("Vekvessætra"), rs.getInt("Øvensenget"));
+            	inventory.add(wood);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return inventory;
+    }	
 	
 	public static List<ModelEquipment> getEquipment() {
         String sql = "select * from current_inventory2 where utstyr <> 'wood' and utstyr <> 'status'  and utstyr <> 'smoke' order by utstyr";
